a6349e152079fd334973322e327e4fb2cf9a9d08
ProggerX <x@proggers.ru>
2024-05-17 13:51:31 +0300

    Changed daily notes location

--- home-config/neovim/default.nix
+++ home-config/neovim/default.nix
@@ -71,21 +71,24 @@
 		# Plugins
 		plugins = {
 			alpha.enable = true;
-		obsidian.enable = true;
-		obsidian.settings = {
-	completion = {
-		min_chars = 2;
-		nvim_cmp = true;
-	};
-	new_notes_location = "current_dir";
-	workspaces = [
-		{
-			name = "default";
-			path = "~/zettle";
-		}
-	];
-};
-		indent-blankline.enable = true;
+			obsidian.enable = true;
+			obsidian.settings = {
+				completion = {
+					min_chars = 2;
+					nvim_cmp = true;
+				};
+				new_notes_location = "current_dir";
+				workspaces = [
+					{
+						name = "default";
+						path = "~/zettle";
+					}
+				];
+				daily_notes = {
+				    folder = "Daily";
+				};
+			};
+			indent-blankline.enable = true;
 			telescope.enable = true;
 			flash.enable = true;
 			persistence.enable = true;
