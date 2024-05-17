1fae4c55779d0dc7a7dd26b0ba67f3541d84dd36
ProggerX <x@proggers.ru>
2024-05-17 21:41:15 +0300

    Fixed sha

--- home-config/emanote/default.nix
+++ home-config/emanote/default.nix
@@ -1,6 +1,9 @@
 { ... }:
 let
-	emanote = import (builtins.fetchTarball "https://github.com/srid/emanote/archive/master.tar.gz");
+	emanote = import (builtins.fetchTarball {
+		url = "https://github.com/srid/emanote/archive/master.tar.gz";
+		sha256 = "sha256:0haj8wxdgjy4ks8kpaaiz5pg65s2l4x44hai5l6sknfidnqfxfj6";
+	});
 in {
 	imports = [ emanote.homeManagerModule ];
 	services.emanote = {
