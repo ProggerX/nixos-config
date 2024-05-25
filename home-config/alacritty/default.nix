097338b1c191592244f7e1076f12430449cd5493
ProggerX <x@proggers.ru>
2024-05-25 11:11:31 +0300

    Better opacity

--- home-config/alacritty/default.nix
+++ home-config/alacritty/default.nix
@@ -1,5 +1,5 @@
 { builtins, config, pkgs, ... }:
 {
     programs.alacritty.enable = true;
-    programs.alacritty.settings = { import = [ "/etc/nixos/home-config/alacritty/catppuccin-mocha.toml" ]; window = {opacity = 0.8;}; font = {size = 17; normal = {family = "JetBrainsMono Nerd Font";};};};
+    programs.alacritty.settings = { import = [ "/etc/nixos/home-config/alacritty/catppuccin-mocha.toml" ]; window = {opacity = 0.7;}; font = {size = 17; normal = {family = "JetBrainsMono Nerd Font";};};};
 }
