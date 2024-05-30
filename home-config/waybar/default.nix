80e19da56760c46ca60f1b31ed70b7d7719ef867
ProggerX <x@proggers.ru>
2024-05-31 00:52:14 +0300

    Waybar ended

--- home-config/waybar/default.nix
+++ home-config/waybar/default.nix
@@ -79,5 +79,138 @@
 	    		max-length = 25;
 			};
 		};
+		style = ''
+			* {
+			    font-family: "JetBrainsMono Nerd Font", FontAwesome, JetBrainsMono, sans-serif;
+			    font-size: 12px;
+				color: #b4befe;
+			}
+			
+			window#waybar {
+			    background-color: rgba(0, 0, 0, 0);
+				border-radius: 10px;
+				padding: 7px;
+			    color: #b4befe;
+			    transition-property: background-color;
+			    transition-duration: .5s;
+			    padding: 0;
+			    margin: 0;
+			}
+			
+			.modules-left > widget:first-child > #workspaces {
+			    margin-left: 0;
+			}
+			
+			.modules-right > widget:last-child > #workspaces {
+			    margin-right: 0;
+			}
+			
+			button {
+			    margin: 0;
+			    padding: 10;
+			    background-color: #1e1e2e;
+			    border-radius: 15;
+			}
+			
+			button:hover {
+			    margin: 0;
+			    padding: 10;
+				color: #1e1e2e;
+			    background-color: #b4befe;
+			    border-radius: 15;
+			}
+			
+			button:focus {
+			    margin: 0;
+			    padding: 10;
+			    background-color: #181825;
+			    border-radius: 15;
+			}
+			
+			#workspaces {
+			    margin: 0;
+			    margin: 5 0 5 0;
+				background-color: #181825;
+				border-radius: 15;
+			}
+			
+			#workspaces button {
+			    padding: 0 5;
+				padding-left: 9px;
+			    margin: 5;
+			    border-radius: 16;
+				background-color: #181825;
+			}
+			
+			#workspaces button:hover {
+				color: #1e1e2e;
+				background-color: #b4befe;
+			}
+			
+			#workspaces button.active {
+			    background-color: #11111b;
+			}
+			
+			#workspaces button.urgent {
+			    background-color: #f38ba8;
+			}
+			
+			#tray {
+			    padding: 7;
+			    margin: 5 0 5 0;
+			    background-color: #181825;
+			    border-radius: 15;
+			}
+			
+			#tray > .passive {
+			    -gtk-icon-effect: dim;
+			}
+			
+			#tray > .needs-attention {
+			    -gtk-icon-effect: highlight;
+			}
+			
+			#custom-alsa {
+			    padding: 0 15 0 15;
+			    margin: 5px 0 5px 0;
+			    background-color: #181825;
+			    border-radius: 15;
+			}
+			
+			#window {
+			    padding: 0 15 0 15;
+			    margin: 5px 0 5px 0;
+			    background-color: #181825;
+			    border-radius: 15;
+			}
+			
+			#clock {
+			    padding: 0 15 0 15;
+			    margin: 5px 0 5px 0;
+			    background-color: #181825;
+			    border-radius: 15;
+			}
+			
+			#mode,
+			#custom-cava,
+			#battery,
+			#language,
+			#custom-weather {
+			    padding: 0 15 0 15;
+			    margin: 5px 0 5px 0;
+			    background-color: #181825;
+			    border-radius: 15;
+			}
+			
+			#keyboard-state {
+			    padding: 0 0px;
+			    margin: 0 5px;
+			    min-width: 16px;
+			}
+			
+			#keyboard-state > label {
+			    padding: 0 0px;
+			}
+		'';
 	};
 };
