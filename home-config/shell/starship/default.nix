8abbca4de3098bb6b99962ab7bfebab28a1b2e5a
ProggerX <x@proggers.ru>
2024-05-31 15:23:21 +0300

    Removed starship palette

--- home-config/shell/starship/default.nix
+++ home-config/shell/starship/default.nix
@@ -1,8 +1,5 @@
-	{ pkgs, ... }: {
+{ ... }: {
   programs.starship =
-    let
-      flavour = "mocha"; # One of `latte`, `frappe`, `macchiato`, or `mocha`
-    in
     {
       enable = true;
       settings = {
@@ -17,14 +14,6 @@
 			vimcmd_symbol = "[vi](bold blue)";
 		};
         format = "$all"; # Remove this line to disable the default prompt format
-        palette = "catppuccin_${flavour}";
-      } // builtins.fromTOML (builtins.readFile
-        (pkgs.fetchFromGitHub
-          {
-            owner = "catppuccin";
-            repo = "starship";
-            rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f"; # Replace with the latest commit hash
-            sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
-          } + /palettes/${flavour}.toml));
     };
+	};
 }
