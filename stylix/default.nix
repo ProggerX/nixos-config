b7b6a4a4c27f15548fa70d03753f9ba44dc0598d
ProggerX <x@proggers.ru>
2024-06-07 23:54:31 +0300

    Tiny formatting

--- stylix/default.nix
+++ stylix/default.nix
@@ -1,38 +1,37 @@
 { pkgs, ... }:
 {
 	stylix = {
-
-	cursor = {
-		name = "Vimix-Cursors";
-		size = 35;
-		package = pkgs.vimix-cursor-theme;
-	};
-	opacity = {
-		terminal = 0.7;
-		applications = 0.8;
-	};
-	fonts = {
-		sizes = {
-			terminal = 17;
+		cursor = {
+			name = "Vimix-Cursors";
+			size = 35;
+			package = pkgs.vimix-cursor-theme;
 		};
-		serif = {
-			package = pkgs.dejavu_fonts;
-			name = "DejaVu Serif";
+		opacity = {
+			terminal = 0.7;
+			applications = 0.8;
 		};
-		sansSerif = {
-			package = (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; });
-			name = "JetBrainsMono Nerd Font";
+		fonts = {
+			sizes = {
+				terminal = 17;
+			};
+			serif = {
+				package = pkgs.dejavu_fonts;
+				name = "DejaVu Serif";
+			};
+			sansSerif = {
+				package = (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; });
+				name = "JetBrainsMono Nerd Font";
+			};
+			monospace = {
+				package = (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; });
+				name = "JetBrainsMono Nerd Font Mono";
+			};
+			emoji = {
+				package = pkgs.noto-fonts-color-emoji;
+				name = "Noto Color Emoji";
+			};
 		};
-		monospace = {
-			package = (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; });
-			name = "JetBrainsMono Nerd Font Mono";
-		};
-		emoji = {
-			package = pkgs.noto-fonts-color-emoji;
-			name = "Noto Color Emoji";
-		};
-	};
-	base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-soft.yaml";
-	image = ./walls.jpg;
+		base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-soft.yaml";
+		image = ./walls.jpg;
 	};
 }
