f7cc8453549c72f2a342ed412032c1e875f46580
ProggerX <x@proggers.ru>
2024-06-29 23:19:47 +0300

    Fixed tabs

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-    description = "Pocket nixos flake";
+	description = "Pocket nixos flake";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		nvimpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		newpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		rust-overlay = {
@@ -10,29 +10,29 @@
 			inputs.nixpkgs.follows = "newpkgs";
 		};
 		nixvim = {
-        	url = "github:nix-community/nixvim";
+			url = "github:nix-community/nixvim";
 			inputs.nixpkgs.follows = "nvimpkgs";
-    	};
-        home-manager = {
-            url = "github:nix-community/home-manager/";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+		};
+		home-manager = {
+			url = "github:nix-community/home-manager/";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 		turnip = {
 			url = "github:ProggerX/turnip";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 		stylix.url = "github:danth/stylix";
-    };
+	};
 
-    outputs = { nixpkgs, stylix, home-manager, ...}@inputs:
+	outputs = { nixpkgs, stylix, home-manager, ...}@inputs:
 		let system = "x86_64-linux";
 		in {
-        nixosConfigurations =
+		nixosConfigurations =
 		let base = [
 			home-manager.nixosModules.home-manager
 			inputs.turnip.nixosModules.turnip
 			stylix.nixosModules.stylix
-            ./configuration.nix
+			./configuration.nix
 			./stylix
 			./turnip
 
@@ -53,25 +53,25 @@
 				];
 			})
 		]; in {
-            pocket-os = nixpkgs.lib.nixosSystem {
-                system = "${system}";
-                
-                modules = base ++ [
+			pocket-os = nixpkgs.lib.nixosSystem {
+				system = "${system}";
+				
+				modules = base ++ [
 					./gaming.nix
 					{
 						imports = [ ./hardware-configuration.nix ];
 					}
-                ];
-            };
-            laptop = nixpkgs.lib.nixosSystem {
-                system = "${system}";
-                modules = base ++ [
+				];
+			};
+			laptop = nixpkgs.lib.nixosSystem {
+				system = "${system}";
+				modules = base ++ [
 					({ lib, ... }: {
 						networking.hostName = lib.mkForce "laptop";
 						imports = [ ./laptop-hardware-configuration.nix ];
 					})
-                ];
-            };
+				];
+			};
 		};
-    };
+	};
 }
