50e4041e127d53c2acc0ae14837c73a809da4a94
ProggerX <x@proggers.ru>
2024-06-30 22:14:35 +0300

    Fix it

--- modules/home.nix
+++ modules/home.nix
@@ -3,6 +3,6 @@
 	home-manager.useGlobalPkgs = true;
 	home-manager.useUserPackages = true;
 	home-manager.backupFileExtension = "old";
-	home-manager.users.proggerx = ./home;
+	home-manager.users.proggerx = ../home;
 	home-manager.extraSpecialArgs = { inherit inputs; };
 }
