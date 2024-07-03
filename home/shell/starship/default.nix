1fe76315a665b6fd4957038a6ec386f57493cca9
ProggerX <x@proggers.ru>
2024-07-03 13:43:01 +0300

    Fix

--- home/shell/starship/default.nix
+++ home/shell/starship/default.nix
@@ -8,16 +8,16 @@
 			format = "[](color_orange)$os$username[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)$c$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:color_blue bg:color_bg3)$docker_context$conda[](fg:color_bg3 bg:color_bg1)$time[ ](fg:color_bg1)$line_break$character";
 
 			palette = "gruvbox_dark";
-			gruvbox_dark.color_fg0 = "#fbf1c7";
-			gruvbox_dark.color_bg1 = "#3c3836";
-			gruvbox_dark.color_bg3 = "#665c54";
-			gruvbox_dark.color_blue = "#458588";
-			gruvbox_dark.color_aqua = "#689d6a";
-			gruvbox_dark.color_green = "#98971a";
-			gruvbox_dark.color_orange = "#d65d0e";
-			gruvbox_dark.color_purple = "#b16286";
-			gruvbox_dark.color_red = "#cc241d";
-			gruvbox_dark.color_yellow = "#d79921";
+			palettes.gruvbox_dark.color_fg0 = "#fbf1c7";
+			palettes.gruvbox_dark.color_bg1 = "#3c3836";
+			palettes.gruvbox_dark.color_bg3 = "#665c54";
+			palettes.gruvbox_dark.color_blue = "#458588";
+			palettes.gruvbox_dark.color_aqua = "#689d6a";
+			palettes.gruvbox_dark.color_green = "#98971a";
+			palettes.gruvbox_dark.color_orange = "#d65d0e";
+			palettes.gruvbox_dark.color_purple = "#b16286";
+			palettes.gruvbox_dark.color_red = "#cc241d";
+			palettes.gruvbox_dark.color_yellow = "#d79921";
 
 			os.disabled = false;
 			os.symbols.NixOS = "";
