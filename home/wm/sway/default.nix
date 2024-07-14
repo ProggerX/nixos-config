318c06019680554c5ad0d16ba41afafa1140025f
ProggerX <x@proggers.ru>
2024-07-14 09:16:21 +0300

    Fixed spotify in sway

--- home/wm/sway/default.nix
+++ home/wm/sway/default.nix
@@ -54,8 +54,8 @@
 			assigns = {
 				"2" = [{ app_id = "^firefox$"; }];
 				"3" = [{ class = "^vesktop$"; }];
-				"0" = [{ class = "^Spotify$"; }];
 				"4" = [{ app_id = "^org.telegram.desktop$"; }];
+				"10" = [{ class = "^Spotify$"; }];
 			};
 		};
 		extraConfig = ''
