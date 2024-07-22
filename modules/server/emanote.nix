3043c70ad7b1a0c12be7824945b3bc97e5c23b0c
ProggerX <x@proggers.ru>
2024-07-22 23:57:51 +0300

    Fixed systemd

--- modules/server/emanote.nix
+++ modules/server/emanote.nix
@@ -4,6 +4,7 @@
 	};
 	config = lib.mkIf config.server.emanote.enable {
 		systemd.services.emanote = {
+			wantedBy = [ "multi-user.target" ];
 			serviceConfig = {
 				WorkingDirectory = "/var/lib/emanote";
 				ExecStart = "${inputs.emanote.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/emanote run --port 8585";
