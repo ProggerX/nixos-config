03a43741e6b48334103535e6fdfb271b932d4ceb
ProggerX <x@proggers.ru>
2024-07-23 00:09:31 +0300

    Added frp for emanote

--- modules/server/emanote.nix
+++ modules/server/emanote.nix
@@ -10,5 +10,14 @@
 				ExecStart = "${inputs.emanote.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/emanote run --port 8585";
 			};
 		};
+		services.frp.settings.proxies = [
+			{
+				type = "tcp";
+				name = "emanote";
+				localPort = 8585;
+				remotePort = 8585;
+				localIP = "127.0.0.1";
+			}
+		];
 	};
 }
