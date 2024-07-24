3f688114dbf31c5125dd86b3c479ae2a32296f9c
ProggerX <x@proggers.ru>
2024-07-24 23:29:43 +0300

    Commit

--- hosts/pocket-os/configuration.nix
+++ hosts/pocket-os/configuration.nix
@@ -48,7 +48,7 @@
 		boot.supportedFilesystems = [ "ntfs" ];
 		
 		boot.extraModulePackages = [ config.boot.kernelPackages.rtl88x2bu ];
-		boot.kernelModules = [ "nouveau" "uinput" ];
+		boot.kernelModules = [ "nouveau" ];
 		
 		networking.networkmanager.enable = true;	
 		nix.settings.experimental-features = ["nix-command" "flakes"];
