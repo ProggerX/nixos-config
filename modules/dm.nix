ea6cf64f424d328a97fe98a97fafb98b82b0dbc7
ProggerX <x@proggers.ru>
2024-07-26 21:43:06 +0300

    Maybe fix

--- modules/dm.nix
+++ modules/dm.nix
@@ -1,15 +1,13 @@
 { pkgs, ... }:
 {
-	systemd.services.greetd.serviceConfig.After = "multi-user.target";
+	boot.kernelParams = [ "console=tty1" ];
 	services.greetd = {
 		enable = true;
+		vt = 2;
 		settings = {
 			default_session = {
 				command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway -r ";
 				user = "greeter";
-			};
-			terminal = {
-				vt = 3;
 			};
 		};
 	};
