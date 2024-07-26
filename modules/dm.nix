96d2bc73839cd6b6e207f1743ddf5bcee780f3c7
ProggerX <x@proggers.ru>
2024-07-26 21:29:12 +0300

    Fixed DM

--- modules/dm.nix
+++ modules/dm.nix
@@ -4,8 +4,11 @@
 		enable = true;
 		settings = {
 			default_session = {
-				command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+				command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway -r ";
 				user = "greeter";
+			};
+			terminal = {
+				vt = 3;
 			};
 		};
 	};
