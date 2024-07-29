987e51d443d571067698a39e7b9936f5bf359910
ProggerX <x@proggers.ru>
2024-07-29 13:05:56 +0300

    Fixed now playing

--- home/wm/waybar/spotify.nix
+++ home/wm/waybar/spotify.nix
@@ -2,9 +2,9 @@
 pkgs.writeShellScriptBin "spotify_now_playing" ''
 	while true
 	do
-		player_status=''$(${pkgs.playerctl}/bin/playerctl status -p spotify 2> /dev/null)
+		player_status=''$(${pkgs.playerctl}/bin/playerctl status -p chromium 2> /dev/null)
 		if [ "$player_status" = "Playing" ]; then
-			echo "''$(${pkgs.playerctl}/bin/playerctl metadata artist -p spotify) - ''$(${pkgs.playerctl}/bin/playerctl metadata title -p spotify)"
+			echo "''$(${pkgs.playerctl}/bin/playerctl metadata artist -p chromium) - ''$(${pkgs.playerctl}/bin/playerctl metadata title -p chromium)"
 		else
 			echo "Nothing is playing"
 		fi
