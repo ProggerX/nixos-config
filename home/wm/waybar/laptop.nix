d3cc125d7f781796563131ae7490c81efaca1c9b
ProggerX <x@proggers.ru>
2024-07-30 21:50:20 +0300

    Deleted cava from laptop

--- home/wm/waybar/laptop.nix
+++ home/wm/waybar/laptop.nix
@@ -1,4 +1,5 @@
 { lib, sys, ... }: 
 lib.mkIf sys.isLaptop {
 	programs.waybar.settings.bar.modules-right = lib.mkForce ["tray" "cpu" "memory" "custom/pipewire" "custom/brightness" "custom/battery" "sway/language" "clock"];
+	programs.waybar.settings.bar.modules-center = lib.mkForce ["custom/spotify"];
 }
