6898eb98aab658fa501e97c67a5694114e5dc8ef
ProggerX <x@proggers.ru>
2024-07-30 14:06:15 +0300

    Commit

--- modules/rust.nix
+++ modules/rust.nix
@@ -1,8 +1,6 @@
 { pkgs, inputs, ... }: {
 	nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
 	environment.systemPackages = [
-		(pkgs.rust-bin.selectLatestNightlyWith(toolchain: toolchain.default.override {
-			extensions = [ "rust-analyzer" ];
-		}))
+		pkgs.rust-bin.stable.latest.default
 	];
 }
