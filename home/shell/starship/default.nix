aec883cd7d84c7a7c93ec18b45d2eda5c2cb8422
ProggerX <x@proggers.ru>
2024-07-30 15:59:50 +0300

    Test

--- home/shell/starship/default.nix
+++ home/shell/starship/default.nix
@@ -5,13 +5,6 @@
 			add_newline = true;
 			lua.disabled = true;
 			c.detect_extensions = ["cpp" "c" "h" "hpp"];
-			character.disabled = false;
-			character.success_symbol = "[󱞪](bold fg:color_green)";
-			character.error_symbol = "[󱞪](bold fg:color_red)";
-			character.vimcmd_symbol = "[](bold fg:color_green)";
-			character.vimcmd_replace_one_symbol = "[](bold fg:color_purple)";
-			character.vimcmd_replace_symbol = "[](bold fg:color_purple)";
-			character.vimcmd_visual_symbol = "[](bold fg:color_yellow)";
 		};
 	};
 }
