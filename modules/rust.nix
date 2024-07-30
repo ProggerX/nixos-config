046e12827562529f9032013e59399f434e4ae85b
ProggerX <x@proggers.ru>
2024-07-30 14:20:19 +0300

    Hz

--- modules/rust.nix
+++ modules/rust.nix
@@ -1,6 +1,8 @@
 { pkgs, inputs, ... }: {
 	nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
 	environment.systemPackages = [
-		pkgs.rust-bin.stable.latest.default
+		pkgs.rust-bin.stable.latest.default.override {
+			extensions = [ "rust-analyzer" "rust-src" ];
+		}
 	];
 }
