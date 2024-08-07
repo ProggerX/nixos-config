79aee050ba69d0ad80f7709c43aea8ce2d6a471d
ProggerX <x@proggers.ru>
2024-08-07 17:57:59 +0300

    Added comma

--- modules/base.nix
+++ modules/base.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
 	imports = [
 		inputs.stylix.nixosModules.stylix
 		inputs.nur.nixosModules.nur
@@ -6,4 +6,5 @@
 		./rust.nix
 		./starship
 	];
+	config.environment.systemPackges = [ pkgs.comma ];
 }
