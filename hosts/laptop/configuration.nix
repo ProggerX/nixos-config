4c8c9e32b544e497cd9bbf525b00a4d8b73f10cb
ProggerX <x@proggers.ru>
2024-08-14 18:33:50 +0300

    Fixed laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -4,10 +4,6 @@
 	
 	services.xserver.videoDrivers = [ "intel" ];
 
-	programs.sway.enable = true;
-	programs.hyprland.enable = lib.mkForce false;
-	services.greetd.settings.default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway -r ";
-
 	services.thermald.enable = true;
 	isLaptop = true;
 }
