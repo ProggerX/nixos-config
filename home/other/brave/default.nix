937067e2ed5dd2a578ca3db12c55e9088a5c9e3f
ProggerX <x@proggers.ru>
2024-08-23 22:17:27 +0300

    Added some extensions

--- home/other/brave/default.nix
+++ home/other/brave/default.nix
@@ -1,5 +1,12 @@
 { ... }: {
     programs.brave = {
         enable = true;
+        extensions = [
+            { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; }
+            { id = "oldceeleldhonbafppcapldpdifcinji"; }
+            { id = "pghmfgnakhjiphmlcnhfpgopkcjhiedc"; }
+            { id = "oofgbpoabipfcfjapgnbbjjaenockbdp"; }
+            { id = "oadlabdleegopgjlkcmjjogeaceagbie"; }
+        ];
     };
 }
