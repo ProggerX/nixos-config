4be475d6f8223c8dda6e7cb407f338a286aa5823
ProggerX <x@proggers.ru>
2024-08-27 12:11:34 +0300

    Fixed music

--- home/wm/waybar/music.nix
+++ home/wm/waybar/music.nix
@@ -2,7 +2,7 @@
 pkgs.writeShellScriptBin "music_now_playing" ''
     while true
     do
-        echo "$(${pkgs.mpc-cli}/bin/mpc | head -n 1 | sed 's/volume:.*/Nothing is playing/')"
+        echo "$(${pkgs.mpc-cli}/bin/mpc | head -n 1 | sed 's/volume:.*//')"
         sleep 1
     done
 ''
