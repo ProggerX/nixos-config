df5596e3682e43a8981181b45246b1b8b3b640e7
ProggerX <x@proggers.ru>
2024-09-15 00:38:52 +0300

    Some tp fixes

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -6,7 +6,10 @@
 	services.xserver.wacom.enable = true;
 	hardware.trackpoint.enable = true;
 	hardware.trackpoint.emulateWheel = true;
+	hardware.trackpoint.fakeButtons = true;
 	hardware.sensor.iio.enable = true;
 	programs.iio-hyprland.enable = true;
+	boot.loader.grub.efiInstallAsRemovable = lib.mkForce false;
+    boot.loader.efi.canTouchEfiVariables = lib.mkForce true;
     isLaptop = true;
 }
