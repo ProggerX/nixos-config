8e7a6281007ccb5fe4c7be25e3d07a2157d4c616
ProggerX <x@proggers.ru>
2024-11-24 11:17:23 +0300

    Migrated to gruvbox-material

--- home/wm/rofi/default.nix
+++ home/wm/rofi/default.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }: {
-    programs.rofi = {
-        enable = true;
-        package = pkgs.rofi-wayland;
-    };
+	stylix.targets.rofi.enable = false;
+	programs.rofi = {
+		enable = true;
+		package = pkgs.rofi-wayland;
+		theme = "${./theme.rasi}";
+		extraConfig = {
+			display-ssh = " ";
+			display-run = " ";
+			display-drun = " ";
+			display-window = " ";
+			display-combi = " ";
+			show-icons = true;
+		};
+	};
 }
