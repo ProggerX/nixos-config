d9bc382a6359c42f0d2c5a69f53a6fa1f36f7547
ProggerX <x@proggers.ru>
2024-11-26 10:55:27 +0300

    Ignore power button on tp

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -5,6 +5,7 @@
     services.xserver.videoDrivers = [ "intel" ];
 	services.xserver.wacom.enable = true;
 	services.xserver.desktopManager.cinnamon.enable = true;
+	services.logind.powerKey = lib.mkForce "ignore";
 	hardware.sensor.iio.enable = true;
 	programs.iio-hyprland.enable = true;
 	boot.loader.grub.efiInstallAsRemovable = lib.mkForce false;
