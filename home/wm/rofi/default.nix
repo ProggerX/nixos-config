4c84cf8a1376619ede6c2bc0abc85f7b8330d5be
ProggerX <x@proggers.ru>
2024-11-28 15:30:04 +0300

    rice: New rofi theme

--- home/wm/rofi/default.nix
+++ home/wm/rofi/default.nix
@@ -5,121 +5,130 @@
 		package = pkgs.rofi-wayland;
 		theme = with sys.lib.stylix.colors; let filename = "theme-rofi.rasi"; in "${pkgs.writeText filename ''
 			* {
-				bg0: #${base00};
-				bg1: #${base01};
-				grey0: #${base02};
-				fg0: #${base05};
-
-				blue: #${base0D};
-				red: #${base08};
-				orange: #${base09};
-				green: #${base0B};
-				bgStatusline3: #${base05};
-
-				background-color: @bg0;
+			    bg-col:  #${base00};
+			    bg-col-light: #${base01};
+			    border-col: #${base05};
+			    selected-col: #${base02};
+			    blue: #${base0D};
+			    fg-col: #${base05};
+			    fg-col2: #${base05};
+			    grey: #${base05};
+			
+			    width: 600;
+			    font: "JetBrainsMono Nerd Font 14";
 			}
-
+			
+			element-text, element-icon , mode-switcher {
+			    background-color: inherit;
+			    text-color:       inherit;
+			}
+			
 			window {
-				height: 600;
-				width: 600;
-
-				border: 1;
-				border-radius: 10;
-				border-color: @bgStatusline3;
+			    height: 540px;
+			    border: 0px;
+			    border-color: @border-col;
+			    background-color: @bg-col;
+			    border-radius: 10px;
 			}
-
+			
 			mainbox {
-				spacing: 0;
-				children: [inputbar, message, listview];
+			    background-color: @bg-col;
 			}
-
+			
 			inputbar {
-				color: @fg0;
-				padding: 14;
-				background-color: @bg0;
+			    children: [prompt,entry];
+			    background-color: @bg-col;
+			    border-radius: 5px;
+			    padding: 2px;
 			}
-
-			message {
-				padding: 10;
-				background-color: @grey0;
-			}
-
-			listview {
-				padding: 8;
-				border-radius: 0 0 10 10;
-				border: 2 2 2 2;
-				border-color: @bg0;
-				background-color: @bg0;
-				dynamic: false;
-			}
-
-			textbox {
-				text-color: @fg0;
-				background-color: inherit;
-			}
-
-			error-message {
-				border: 20 20 20 20;
-			}
-
-			entry, prompt, case-indicator {
-				text-color: inherit;
-			}
-
+			
 			prompt {
-				margin: 0 10 0 0;
+			    background-color: @blue;
+			    padding: 6px;
+			    text-color: @bg-col;
+			    border-radius: 3px;
+			    margin: 20px 0px 0px 20px;
 			}
-
+			
+			textbox-prompt-colon {
+			    expand: false;
+			    str: ":";
+			}
+			
+			entry {
+			    padding: 6px;
+			    margin: 20px 0px 0px 10px;
+			    text-color: @fg-col;
+			    background-color: @bg-col;
+			}
+			
+			listview {
+			    border: 0px 0px 0px;
+			    padding: 6px 0px 0px;
+			    margin: 10px 0px 0px 20px;
+			    columns: 2;
+			    lines: 5;
+			    background-color: @bg-col;
+			}
+			
 			element {
-				padding: 5;
-				vertical-align: 0.5;
-				border-radius: 10;
-				background-color: @bg1;
+			    padding: 5px;
+			    background-color: @bg-col;
+			    text-color: @fg-col  ;
+			    border-radius: 5px;
 			}
-
-			element.selected.normal {
-				background-color: @grey0;
+			
+			element-icon {
+			    size: 25px;
 			}
-
-			element.alternate.normal {
-				background-color: inherit;
+			
+			element selected {
+			    background-color:  @selected-col ;
+			    text-color: @fg-col2  ;
 			}
-
-			element.normal.active, element.alternate.active {
-				background-color: @orange;
+			
+			mode-switcher {
+			    spacing: 0;
+			  }
+			
+			button {
+			    padding: 10px;
+			    background-color: @bg-col-light;
+			    text-color: @grey;
+			    vertical-align: 0.5;
+			    horizontal-align: 0.5;
 			}
-
-			element.selected.active {
-				background-color: @green;
+			
+			button selected {
+			  background-color: @bg-col;
+			  text-color: @blue;
 			}
-
-			element.normal.urgent, element.alternate.urgent {
-				background-color: @red;
+			
+			message {
+			    background-color: @bg-col-light;
+			    margin: 2px;
+			    padding: 2px;
+			    border-radius: 5px;
 			}
-
-			element.selected.urgent {
-				background-color: @blue;
-			}
-
-			element-text, element-icon {
-				size: 40;
-				margin: 0 10 0 0;
-				vertical-align: 0.5;
-				background-color: inherit;
-				text-color: @fg0;
-			}
-
-			element-text .active, element-text .urgent {
-				text-color: @bg0;
+			
+			textbox {
+			    padding: 6px;
+			    margin: 20px 0px 0px 20px;
+			    text-color: @blue;
+			    background-color: @bg-col-light;
 			}
 		''}";
 		extraConfig = {
-			display-ssh = " ";
-			display-run = " ";
-			display-drun = " ";
-			display-window = " ";
-			display-combi = " ";
-			show-icons = true;
+			show-icons = false;
+    		modi = "run,drun,window";
+    		terminal = "kitty";
+    		drun-display-format = "{icon} {name}";
+    		disable-history = false;
+    		hide-scrollbar = true;
+    		display-drun = "   Apps ";
+    		display-run = "   Run ";
+    		display-window = " 󰕰  Window ";
+    		sidebar-mode = true;
 		};
 	};
 }
