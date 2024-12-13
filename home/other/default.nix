a400609dc2f026130e0276320d8132058e345cf7
ProggerX <x@proggers.ru>
2024-12-13 12:02:31 +0300

    style: Add icon theme

--- home/other/default.nix
+++ home/other/default.nix
@@ -1,8 +1,14 @@
-{
+{ pkgs, ... }: {
     imports = [
         ./zen
         ./forkgram
         ./spotify
 		./tor-proxy
     ];
+	stylix.iconTheme = {
+		enable = true;
+		package = pkgs.gruvbox-plus-icons;
+		dark = "Gruvbox-Plus-Dark";
+		light = "Gruvbox-Plus-Light";
+	};
 }
