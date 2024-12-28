5f56948a4530a74ca02cd8695163d1c73b5793e8
ProggerX <x@proggers.ru>
2024-12-28 19:38:28 +0300

    Changed fetch logo

--- home/shell/neofetch/default.nix
+++ home/shell/neofetch/default.nix
@@ -1,7 +1,7 @@
 { ... }: {
     xdg.configFile."neofetch/config.conf".source = ./config.conf;
-    xdg.configFile."neofetch/logo.jpg" = {
-        source = ./logo.jpg;
+    xdg.configFile."neofetch/logo.txt" = {
+        source = ./logo.txt;
         executable = true;
         recursive = true;
     };
