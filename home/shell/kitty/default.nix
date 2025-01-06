f0cf93d59609b02e0982928aac0e59cc6ec2b3de
ProggerX <x@proggers.ru>
2025-01-06 19:41:32 +0300

    Disabled kitty bell

--- home/shell/kitty/default.nix
+++ home/shell/kitty/default.nix
@@ -3,6 +3,9 @@ let unstable = import inputs.unstable { inherit system; }; in {
     programs.kitty = {
 		enable = true;
 		package = unstable.kitty;
-		settings.cursor_trail = 3;
+		settings = {
+			cursor_trail = 3;
+			enable_audio_bell = "no";
+		};
 	};
 }
