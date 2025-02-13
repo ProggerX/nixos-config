ea5df6a8908a67606a6901a59173112e8094f35b
ProggerX <x@proggers.ru>
2025-02-13 19:47:02 +0300

    Add mopidy-mpris

--- modules/mopidy.nix
+++ modules/mopidy.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, ... }: {
 	services.mopidy = {
 		enable = true;
-		extensionPackages = [ pkgs.mopidy-mpd pkgs.mopidy-subidy ];
+		extensionPackages = with pkgs; [ mopidy-mpd mopidy-subidy mopidy-mpris ];
 		extraConfigFiles = [ "/mopidy.conf" ];
 	};
 	systemd.services.mopidy = {
