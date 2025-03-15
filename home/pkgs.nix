e143be7f35d620619a4818de99375a20dfa33226
ProggerX <x@proggers.ru>
2025-03-15 19:33:10 +0300

    Add gamescope

--- home/pkgs.nix
+++ home/pkgs.nix
@@ -2,6 +2,7 @@
 let unstable = import inputs.unstable { inherit system; }; in {
     home.packages = with pkgs; [
 		apostrophe
+		gamescope
 		nicotine-plus
 		fretboard
 		haskellPackages.pointfree
