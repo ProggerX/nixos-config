5c9cbe4e0c14a9fec3e0939fea9f8c55990a6e80
ProggerX <x@proggers.ru>
2025-03-24 23:39:58 +0300

    Recompile ncmpcpp only on PC

--- home/other/ncmpcpp/default.nix
+++ home/other/ncmpcpp/default.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, sys, ... }: {
 	programs.ncmpcpp = {
 		enable = true;
-		package = pkgs.ncmpcpp.override {visualizerSupport = true;};
+		package = if !sys.isLaptop then pkgs.ncmpcpp.override {visualizerSupport = true;} else pkgs.ncmpcpp;
 		settings = {
 			lyrics_fetchers = "tags, plyrics";
 			user_interface = "alternative";
