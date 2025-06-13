2e516ee2013af2402881920a339c959afa731e34
ProggerX <x@proggers.ru>
2025-06-13 17:58:57 +0300

    Add sleepy launcher

--- modules/aagl.nix
+++ modules/aagl.nix
@@ -2,4 +2,5 @@
     imports = [ inputs.aagl.nixosModules.default ];
 	nix.settings = inputs.aagl.nixConfig;
 	programs.honkers-railway-launcher.enable = true;
+	programs.sleepy-launcher.enable = true;
 }
