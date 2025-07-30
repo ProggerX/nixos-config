154bcd835a366a1d6b3abb2e403f2f763d149939
Keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-07-30 11:12:24 +0300

    Update wf-recorder.nix

--- home/wm/waybar/wf-recorder.nix
+++ home/wm/waybar/wf-recorder.nix
@@ -1,4 +1,9 @@
-{ pkgs, symlinkJoin, writeShellScriptBin }:
+{
+	wf-recorder,
+	slurp,
+	symlinkJoin,
+	writeShellScriptBin
+}:
 let
 	status = writeShellScriptBin "wf-status" ''
 		if pgrep wf-rec &> /dev/null; then
@@ -14,10 +19,10 @@ let
 			saves_dir=$HOME/wf-records/
 			save_file=$saves_dir/$(date +"%F_%H-%M-%S").mp4
 			mkdir -p $saves_dir
-			${pkgs.wf-recorder}/bin/wf-recorder \
+			${wf-recorder}/bin/wf-recorder \
 				-c libx264 \
 				-r 25 \
-				-g "$(${pkgs.slurp}/bin/slurp)" \
+				-g "$(${slurp}/bin/slurp)" \
 				-f $save_file \
 				-p qp=0 &>/dev/null &
 		fi
