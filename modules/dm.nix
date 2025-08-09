b2972eac6fdcab0081b8e0f0425b1949227392ea
ProggerX <x@proggers.ru>
2025-08-09 16:22:45 +0300

    Autologin

--- modules/dm.nix
+++ modules/dm.nix
@@ -1,14 +1,15 @@
-{ pkgs, ... }:
+{ ... }:
 {
-    boot.kernelParams = [ "console=tty1" ];
-    services.greetd = {
-        enable = true;
-        vt = 2;
-        settings = {
-            default_session = {
-                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland -r ";
-                user = "greeter";
-            };
-        };
-    };
+	boot.kernelParams = [ "console=tty1" ];
+	services.greetd = {
+		enable = true;
+		vt = 2;
+		settings = {
+			default_session = {
+				# command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland -r ";
+				command = "Hyprland";
+				user = "proggerx";
+			};
+		};
+	};
 }
