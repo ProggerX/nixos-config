43b83344f14143e38e9369dcaca74c8b3c3a27f9
ProggerX <x@proggers.ru>
2025-09-21 18:31:49 +0300

    Change hypridle behaviour

--- home/wm/hypridle/default.nix
+++ home/wm/hypridle/default.nix
@@ -1,10 +1,14 @@
-{ pkgs, ... }: {
-	services.hypridle = {
+{ pkgs, ... }:
+let enableSound = false;
+	close-sound = if enableSound then " && ${pkgs.mpv}/bin/mpv ${./minecraft-doors-close.mp3}" else "";
+	open-sound = "${pkgs.mpv}/bin/mpv ${./minecraft-doors-open.mp3}";
+in {
+		services.hypridle = {
 		enable = true;
 		settings = {
 			general = {
-				before_sleep_cmd = "${pkgs.mpv}/bin/mpv ${./minecraft-doors-close.mp3} && hyprlock --no-fade-in";
-				after_sleep_cmd = "${pkgs.mpv}/bin/mpv ${./minecraft-doors-open.mp3}";
+				before_sleep_cmd = "hyprlock --no-fade-in" + close-sound;
+				after_sleep_cmd = if enableSound then open-sound else "";
 				lock_cmd = "hyprlock --no-fade-in";
 			};
 		};
