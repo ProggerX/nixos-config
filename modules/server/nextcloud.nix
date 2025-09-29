dc51227450dad23289bbc3d7047a040d73e80318
ProggerX <x@proggers.ru>
2025-09-29 11:01:17 +0300

    Enable nc https

--- modules/server/nextcloud.nix
+++ modules/server/nextcloud.nix
@@ -5,9 +5,14 @@
 		hostName = "nc.bald.su";
 		config.adminpassFile = "/nextcloud-pass";
 		config.dbtype = "sqlite";
+		https = true;
 		extraApps = {
 			inherit (config.services.nextcloud.package.packages.apps) calendar tasks;
 		};
 		extraAppsEnable = true;
+	};
+	services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+		forceSSL = true;
+		enableACME = true;
 	};
 }
