ae7aec8abc14c59ee90f72be3bc519fca81013e2
ProggerX <x@proggers.ru>
2025-09-30 22:18:59 +0300

    Add collabora

--- modules/server/nextcloud.nix
+++ modules/server/nextcloud.nix
@@ -1,5 +1,29 @@
 { config, ... }:
 {
+	virtualisation.oci-containers.containers.collabora = {
+		image = "docker.io/collabora/code";
+		ports = [ "9980:9980" ];
+		autoStart = true;
+		environment = {
+			# This limits it to this NC instance AFAICT
+			aliasgroup1 = "https://nc.bald.su:443";
+			# Must disable SSL as it's behind a reverse proxy
+			extra_params = "--o:ssl.enable=false";
+		};
+	};
+	services.nginx = {
+		enable = true;
+		virtualHosts.office = {
+			addSSL = true;
+			enableACME = true;
+			serverName = "office.bald.su";
+			locations."/" = {
+				proxyPass = "http://0.0.0.0:9980";
+			};
+			extraConfig = "client_max_body_size 5G;";
+		};
+	};
+
 	services.nextcloud = {
 		enable = true;
 		hostName = "nc.bald.su";
