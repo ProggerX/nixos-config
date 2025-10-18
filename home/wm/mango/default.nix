c127fdbe088f886477c1019772e089890c47c7fd
ProggerX <x@proggers.ru>
2025-10-18 11:09:09 +0300

    Done configuring mango

--- home/wm/mango/default.nix
+++ home/wm/mango/default.nix
@@ -4,7 +4,18 @@
 	wayland.windowManager.mango = {
 		enable = true;
 		settings = builtins.readFile ./config.conf;
-		autostart_sh = builtins.readFile ./autostart.sh;
+		autostart_sh = 
+			builtins.concatStringsSep "\n" ([
+				(builtins.readFile ./autostart.sh)
+				"${pkgs.wl-clipboard}/bin/wl-paste --watch cliphist store"
+				"firefox-nightly"
+				"${pkgs.telegram-desktop}/bin/Telegram"
+				"sshfs 10.7.0.6:/music /home/proggerx/music"
+				"easyeffects --gapplication-service"
+				"${pkgs.networkmanagerapplet}/bin/nm-applet"
+				] ++ (if sys.isLaptop then [
+					"${pkgs.wlsunset}/bin/wlsunset -T 5500"
+				] else []));
 	};
 	services.swayosd.enable = true;
 }
