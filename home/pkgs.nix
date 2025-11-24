0c4fa7ecdfdb4d7b829f9e28acc7136364d3e32d
ProggerX <x@proggers.ru>
2025-11-24 18:34:19 +0300

    Refactor package set

--- home/pkgs.nix
+++ home/pkgs.nix
@@ -1,5 +1,8 @@
 { pkgs, inputs, system, sys, ... }: {
 home.packages = with pkgs; [
+		(ghc.withPackages (ps:
+			with ps; [ vector containers lens aeson ]
+		))
 		kanshi
 		localsend
 		rlci
@@ -39,141 +42,140 @@ home.packages = with pkgs; [
 		ollama
 		marksman
 		codeberg-cli
-        inputs.pabc-nix.packages.${pkgs.system}.default
+		inputs.pabc-nix.packages.${pkgs.system}.default
 		codeblocks
 		typst
 		tinymist
 		pandoc
-        ghc
 		nodejs
-        cf-tool
-        haskellPackages.hoogle
+		cf-tool
+		haskellPackages.hoogle
 		arduino
 		arduino-cli
 		zig
 		zls
 		arduino-language-server
-        cassette
-        haskell-language-server
-        cabal-install
-        stack
-        cool-retro-term
-        tgpt
-        neovide
-        lutris
-        deploy-rs
-        openvpn3
-        swayimg
-        element-desktop
-        obs-studio
-        fasole
-        wmctrl
-        typescript
-        musikcube
-        mindustry-wayland
-        ags
-        wineWowPackages.full
-        wails
-        restish
-        ffmpeg
-        lazydocker
-        mpv
-        clang-tools
-        mpg123
-        parted
-        aalib
-        exiftool
-        fzf
-        zoxide
-        sshpass
-        simplex-chat-desktop
-        nchat
-        ncdu
-        openboard
-        leetcode-cli
-        speedcrunch
-        nemo
-        tor-browser
-        neofetch
-        prismlauncher
-        bc
-        gimp
-        openjdk17-bootstrap
-        osu-lazer-bin
+		cassette
+		haskell-language-server
+		cabal-install
+		stack
+		cool-retro-term
+		tgpt
+		neovide
+		lutris
+		deploy-rs
+		openvpn3
+		swayimg
+		element-desktop
+		obs-studio
+		fasole
+		wmctrl
+		typescript
+		musikcube
+		mindustry-wayland
+		ags
+		wineWowPackages.full
+		wails
+		restish
+		ffmpeg
+		lazydocker
+		mpv
+		clang-tools
+		mpg123
+		parted
+		aalib
+		exiftool
+		fzf
+		zoxide
+		sshpass
+		simplex-chat-desktop
+		nchat
+		ncdu
+		openboard
+		leetcode-cli
+		speedcrunch
+		nemo
+		tor-browser
+		neofetch
+		prismlauncher
+		bc
+		gimp
+		openjdk17-bootstrap
+		osu-lazer-bin
 		kdePackages.xwaylandvideobridge
-        alsa-utils
-        toilet figlet
-        cowsay
-        glib
-        wf-recorder
-        translate-shell
-        github-cli
-        mono
-        fswatch
-        zathura
-        brightnessctl
-        transmission_3
-        chromium
-        ventoy
-        file
-        ueberzug
-        element
-        viu
-        pyright
-        just
-        tmux
-        ripgrep
-        git
-        lazygit
-        go
-        bear
-        vitetris
-        tty-clock
-        fpc
-        tree
-        btop
-        cava
-        showmethekey
-        jq
-        nixd
-        pulseaudio
-        grim
-        slurp
-        wl-clipboard cliphist
-        waybar
-        winetricks
-        wireplumber
-        libreoffice
-        p7zip
-        ldmtool
-        python311Packages.pip
-        timeshift
-        sonic-pi
-        slstatus
-        zip
-        unzip
-        cmake
-        pamixer
-        gnupg
-        pavucontrol
-        xclip
-        lldb
-        lua-language-server
-        tmux
-        obsidian
-        slurp
-        eza
-        python311
-        vesktop
-        gcc
-        gnumake
-        nitch
-        direnv
+		alsa-utils
+		toilet figlet
+		cowsay
+		glib
+		wf-recorder
+		translate-shell
+		github-cli
+		mono
+		fswatch
+		zathura
+		brightnessctl
+		transmission_3
+		chromium
+		ventoy
+		file
+		ueberzug
+		element
+		viu
+		pyright
+		just
+		tmux
+		ripgrep
+		git
+		lazygit
+		go
+		bear
+		vitetris
+		tty-clock
+		fpc
+		tree
+		btop
+		cava
+		showmethekey
+		jq
+		nixd
+		pulseaudio
+		grim
+		slurp
+		wl-clipboard cliphist
+		waybar
+		winetricks
+		wireplumber
+		libreoffice
+		p7zip
+		ldmtool
+		python311Packages.pip
+		timeshift
+		sonic-pi
+		slstatus
+		zip
+		unzip
+		cmake
+		pamixer
+		gnupg
+		pavucontrol
+		xclip
+		lldb
+		lua-language-server
+		tmux
+		obsidian
+		slurp
+		eza
+		python311
+		vesktop
+		gcc
+		gnumake
+		nitch
+		direnv
 		cartridges
-    ] ++ (if sys.isLaptop then [] else [
+	] ++ (if sys.isLaptop then [] else [
 		(dwarf-fortress-packages.dwarf-fortress-full.override {
-            dfVersion = "0.47.05";
-            enableDwarfTherapist = false;
-        })
+			dfVersion = "0.47.05";
+			enableDwarfTherapist = false;
+		})
 	]);
 }
