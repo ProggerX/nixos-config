0ba8c0cf3165221eca221648071b31ba922b29a0
ProggerX <x@proggers.ru>
2026-02-04 22:42:16 +0300

    setup swayidle

--- home/wm/hypridle/default.nix
+++ home/wm/hypridle/default.nix
@@ -3,8 +3,8 @@ let enableSound = false;
 	close-sound = if enableSound then " && ${pkgs.mpv}/bin/mpv ${./minecraft-doors-close.mp3}" else "";
 	open-sound = "${pkgs.mpv}/bin/mpv ${./minecraft-doors-open.mp3}";
 in {
-		services.hypridle = {
-		enable = true;
+	services.hypridle = {
+		enable = false;
 		settings = {
 			general = {
 				before_sleep_cmd = "hyprlock --no-fade-in" + close-sound;
@@ -12,5 +12,12 @@ in {
 				lock_cmd = "hyprlock --no-fade-in";
 			};
 		};
+	};
+	services.swayidle = {
+		enable = true;
+		events = [{
+			event = "before-sleep";
+			command = "${pkgs.swaylock}/bin/swaylock -f";
+		}];
 	};
 }
