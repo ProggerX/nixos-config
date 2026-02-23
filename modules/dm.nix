b7ccc1ea61d795e10e92f02b32d2a3a7b0c97c8d
ProggerX <x@proggers.ru>
2026-02-23 11:20:41 +0300

    update tuigreet command

--- modules/dm.nix
+++ modules/dm.nix
@@ -1,10 +1,13 @@
-{pkgs, ...}: {
+{pkgs, ...}:
+let compositor = "sway";
+	user = "proggerx";
+in {
   boot.kernelParams = ["console=tty2"];
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time -r --cmd sway";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time -r --cmd '${compositor} &> /home/${user}/.${compositor}.log'";
         user = "greeter";
       };
     };
