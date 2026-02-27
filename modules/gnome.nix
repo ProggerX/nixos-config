201799eeca322d400eec58fa40af63150aa93e7a
ProggerX <x@proggers.ru>
2026-02-27 22:02:05 +0300

    comment out outdated dconf options

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -3,10 +3,10 @@
     {
       lockAll = true;
       settings = {
-        "org/gnome/desktop/input-sources" = {
-          sources = "[('xkb', 'us'), ('xkb', 'ru')]";
-          xkb-options = ["grp:win_space_toggle" "caps:escape"];
-        };
+        # "org/gnome/desktop/input-sources" = {
+        #   sources = "[('xkb', 'us'), ('xkb', 'ru')]";
+        #   xkb-options = ["grp:win_space_toggle" "caps:escape"];
+        # };
         "org/gnome/shell" = {
           enabled-extensions = with pkgs.gnomeExtensions; [
             runcat.extensionUuid
